87046fa59557f76332ce72a4281fa2f8ec5073f9
Thiago Souza <thiago.souza@eposnow.com>
2019-04-19 18:32:55 -0300

    Criação estrutura do projeto com a arquitetura Clean DDD e criação da entidade Cliente.

---
+++ src/TCC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set;  }
+
+        //public List<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ src/TCC.Infrasctructure/Data/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TCC.ApplicationCore.Entity;
+
+namespace TCC.Infrasctructure.Data
+{
+    public class Context : DbContext 
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Cliente");
+        }
+    }
+}
